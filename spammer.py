e78cb9437575c9b70195a919887e5e9ef012c2a6
Shining <146763225+shining3ye@users.noreply.github.com>
2024-08-31 10:52:24 +0600

    put main code spammer.py

--- spammer.py
+++ spammer.py
@@ -1,1 +1,26 @@
+import time
+import pyautogui
 
+# The Thong You want to spam/nuke
+message = "This is a FATMAN!"
+# Time delay between each message
+delay = 0.25  # (0.025-1.5) seconds
+
+# Short delay before the script starts, so you can switch to the input field
+print("Starting in 5 seconds...")
+time.sleep(1)
+print("5")
+time.sleep(1)
+print("4")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+
+# here the msg loop starts
+while True:
+    pyautogui.typewrite(message)
+    pyautogui.press("enter")
+    time.sleep(delay)
